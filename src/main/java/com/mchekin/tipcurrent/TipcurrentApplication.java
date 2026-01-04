9a8bb4c25fd6134e949ae54d15feda866cf99c33
Michael Chekin <mchekin@gmail.com>
2026-01-04 12:19:39 +0100

    Initial commit

---
+++ src/main/java/com/mchekin/tipcurrent/TipcurrentApplication.java
@@ -0,0 +1,13 @@
+package com.mchekin.tipcurrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TipcurrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TipcurrentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mchekin/tipcurrent/TipcurrentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mchekin.tipcurrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TipcurrentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
